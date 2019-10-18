e51230f47104cf622c8b7c4fdfc7511000b1df3e
Angelia Knight <aknight@ucvts.org>
2019-10-18 13:22:27 -0400

    initial commit. me no likey

---
+++ src/area.js
@@ -0,0 +1,5 @@
+const millimetersPerInch = 2.54;
+let readlineSync = require("readline-sync");
+let width = readlineSync.question("\nWidth: ");
+let length = readlineSync.question("Length: ");
+let width*legth;
