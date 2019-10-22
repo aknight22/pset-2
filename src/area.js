4353c0504e1e40d983ead53f4f043b4d9cf2a991
Angelia Knight <aknight@ucvts.org>
2019-10-22 13:25:41 -0400

    excersize 3

--- src/area.js
+++ src/area.js
@@ -1,5 +1,13 @@
-const millimetersPerInch = 2.54;
-let readlineSync = require("readline-sync");
+const readlineSync = require("readline-sync");
+
+const millimetersPerInch = 25.4;
+
 let width = readlineSync.question("\nWidth: ");
 let length = readlineSync.question("Length: ");
-let width*legth;
+
+let widthInMillimeters = width * millimetersPerInch;
+let lengthInMillimeters = length * millimetersPerInch;
+let area = widthInMillimeters * lengthInMillimeters;
+area = area.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+console.log("\nA(n) " + width + "-by-" + length + "-inch sheet of paper has an area of " + area + " square millimeters.");
