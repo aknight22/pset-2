847b65f8da502f7e395a198c5d12d21f12cc34ad
aknight22 <44117063+aknight22@users.noreply.github.com>
2019-10-27 12:09:44 -0400

    onto exercise 9

--- src/salary.js
+++ src/salary.js
@@ -2,4 +2,16 @@ const readlineSync = require("readline-sync");
 
 const annualSalary = readlineSync.question("\nAnnual salary: ");
 
-const 
+const retirement = 0.07; 
+const federal = 0.157; 
+const state = 0.0447;
+const socialSecurity = 0.062;
+const medicare = 0.0145;
+
+const salary = annualSalary - ((annualSalary*retirement) + (annualSalary*federal) + (annualSalary*state) + (annualSalary*socialSecurity) + (annualSalary*medicare));
+
+let biWeekly = salary /24; 
+
+biWeekly= biWeekly.toLocaleString("en", { style: currency, currency: USD});
+
+console.log("Your take-home pay each check will be " + biWeekly + "."
